b1c93a364d24bde16496f66d27ea4078f267b350
alucard1792 <dz9217@hotmail.com>
2019-06-04 21:15:49 -0500

    part 4 - 2. The Markup.mp4

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,7 +26,10 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
-
+        public ActionResult New()
+        {
+            return View();
+        }
         public ActionResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
