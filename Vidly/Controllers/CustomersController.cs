89ceeddb8c5aa37428494b4ee8efaa9bc783d096
alucard1792 <dz9217@hotmail.com>
2019-06-08 11:50:36 -0500

    part 4 - 5. Model Binding.mp4

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -36,7 +36,20 @@ namespace Vidly.Controllers
             };
             return View(viewModel);
         }
-        public ActionResult Index()
+        [HttpPost]
+        /*
+         *mvc framework automatically map request data to the párameter object
+         * this is what we call model binding, so nvc framework binds this model to request data
+         * So if we view the request of the view(f12 -> network), mvc frameworkwill use the properties send to
+         * initialize the model binding in parameter, in this case we used a model view object but if we change to Customer
+         * nvc framework is it smart enough to bind this object to form data becouse all the keys in the form data are prefixed but customer
+         * this is how model binding works  
+         */
+        public ActionResult Create(NewCustomerViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+        public ActionResult Index() 
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
